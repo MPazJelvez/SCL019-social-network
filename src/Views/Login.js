c8bcb2f05b4b0ccfd962d9fb74d94b7c08dd7159
Norma <>
2022-02-23 17:27:20 -0300

    cambios css de todas las vistas js

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -5,14 +5,20 @@ export const login = () => {
   const pageLogin = ` 
     <h1 class ="login-title"> Iniciar Sesion</h1>
        <form class="login-form">
+         <div class="login-input">
            <label for ="userEmail">Correo/ Usuario</label>
            <input type="text" placeholder="ingresa tu correo o nombre de usuario" 
            name ="userEmail">
+          </div>
+          <div class="login-input">
            <label for ="password">Contraseña</label>
            <input type="password" placeholder="ingresa tu Contraseña" 
            name ="password">
+           </div>
+
          <span> ¿Has olvidado tu contraseña?</span>
-         <a href="#">Ingresar</a>
+
+         <a class="btn" href="#">Ingresar</a>
       
        </form>
  
--- src/Views/Register.js
+++ src/Views/Register.js
@@ -6,22 +6,32 @@ export const register = () => {
 
 <h1 class ="register-title"> Registrarse</h1>
        <form class="register-form">
+          <div class="register-input">
            <label for ="Email">Correo</label>
            <input type="email" placeholder="ingresa tu correo" 
            name ="Email">
+          </div>
+          <div class="register-input">
            <label for ="user">Usuario</label>
            <input type="text" placeholder="ingresa tu usuario" 
            name ="user">
+          </div> 
+          <div class="register-input">
            <label for ="password">Contraseña</label>
            <input type="password" placeholder="ingresa tu Contraseña" 
            name ="password">
+          </div>
+          <div class="register-input">
            <label for ="password">Repetir Contraseña</label>
            <input type="password" placeholder="ingresa tu Contraseña" 
            name ="password">
-         <label for="">He leído y acepto los términos y condiciones </label>
-         <input type="checkbox" >
+          </div>
+          <div class="register-input center" >
+       
+         <span > <input type="checkbox"> He leído y acepto los términos y condiciones </span>
+          </div>
 
-         <a href="#/firstPage">Registrarse</a>
+         <a class="btn" href="#/firstPage">Registrarse</a>
       
        </form>
        `;
