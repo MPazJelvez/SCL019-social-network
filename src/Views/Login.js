afe1ecc0a88a5bbaf1185eac258d5e9e4f99d5cd
María Paz Jélvez Herrera <mpazjelvez@gmail.com>
2022-03-01 16:42:58 -0300

    login listo

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,20 +1,20 @@
+import { signIn } from '../lib/index.js';
+
 export const login = () => {
   window.location.hash = '/login';
   const divLogin = document.createElement('div');
   divLogin.className = 'div';
   divLogin.innerHTML = ` 
-    <h1 class ="login-title"> Iniciar Sesion</h1>
+    <h1 class ="login-title"> Iniciar Sesión</h1>
         <div class="error-message">
-        <span class="error"> Ingrese un correo válido </span>
-        <span class="error"> Ingrese un usuario válido </span>
-        <span class="error"> Ingresa una contraseña </span>
-        <span class="error"> No puede haber espacios vacíos </span>
-        <span class="error"> Completar este campo para poder continuar </span>
+        <span class="error" id="invalidEmail"> Ingrese un correo válido </span>
+        <span class="error hidden" id="invalidPassword"> Ingresa una contraseña </span>
+        <span class="error hidden" id="noSpace"> No puede haber espacios vacíos </span>
         </div>
        <form class="login-form">
          <div class="login-input">
-           <label for="userEmail">Correo/ Usuario</label>
-           <input type="text" placeholder="ingresa tu correo o nombre de usuario" 
+           <label for="userEmail">Correo</label>
+           <input type="text" placeholder="ingresa tu correo" 
            name="userEmail" id="userEmail">
           </div>
           <div class="login-input">
@@ -23,7 +23,7 @@ export const login = () => {
            name ="password" id="password">
            </div>
          <span> ¿Has olvidado tu contraseña?</span>
-         <a class="btn" href="#/firstPage" id="login">Ingresar</a>
+         <a class="btn" href="#" id="login">Ingresar</a>
        </form>
  
    `;
@@ -31,7 +31,7 @@ export const login = () => {
   divLogin.querySelector('#login').addEventListener('click', () => {
     const emailInput = document.querySelector('#userEmail').value;
     const passwordInput = document.querySelector('#password').value;
-    // createUser(emailInput, passwordInput);
+   signIn(emailInput, passwordInput);
   });
 
   return divLogin;
