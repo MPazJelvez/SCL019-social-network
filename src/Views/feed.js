a6015c13dd5e1a6f1226d54ec1a994d3a618043f
Norma <>
2022-03-18 12:58:55 -0300

    posteamos, delete,edit, username

--- src/Views/feed.js
+++ src/Views/feed.js
@@ -1,8 +1,9 @@
 
 
 
+import { logOut} from '../lib/index.js';
+import { createPost, onGetPost, getPost, deletePost, editPost } from '../lib/firestore.js'
 
-import { logOut, createPost, onGetPost, getPost, deletePost } from '../lib/index.js';
 
 export const feed = () => {
   window.location.hash = '/feed';
@@ -16,106 +17,112 @@ export const feed = () => {
         <a href="" id="logOut"> Salir 
         <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="currentColor"><path d="M8.514 20h-4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h4v2h-4v12h4v2Z"/><path d="m13.842 17.385l1.42-1.408l-3.92-3.953h9.144a1 1 0 1 0 0-2h-9.162l3.98-3.947l-1.408-1.42l-6.391 6.337l6.337 6.391Z"/></g></svg>
         </a>
-        
-        <form id='post-form'>
+        <div class='form-container'>
+        <form class='post-form' id='post-form'>
           <label for='title'> Titulo </label>
           <input type='text' id='title'>
           <label for='description'> Escribir Post </label>
           <textarea name='description' id="description" cols="0" rows="3"></textarea>
-          <!-- <label for='image'> Inserta una Imagen </label> -->
-          <!-- <input type='file' id='image' name='image'> -->
-          <button id='post-btn'>Postear</button>
-          <button id=''>probando</button>
-        </form>
-        <div id='post-container'></div>
+          <button class='btn' id='post-btn'>Postear</button>
+          </form>
+        </div>
+        <div class='post-container' id='post-container'></div>
         `;
   const postContainer = divFeed.querySelector('#post-container');
   const postForm = divFeed.querySelector('#post-form');
-
+  let editStatus = false;
       
   divFeed.querySelector('#logOut').addEventListener('click', (e) => {
     e.preventDefault();
     logOut();
   });
 
-  postForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const title = divFeed.querySelector('#title').value;
-    const description = divFeed.querySelector('#description').value;
-    createPost(title, description );
-    postForm.reset();
-  });
-  
-  const postBtn = divFeed.querySelector('#post-btn');
-postBtn.addEventListener('click', async (e) => {
-  console.log('holi')
-  });
-
-  if (divFeed != '') {
-    postContainer.innerHTML ='';
-    let postStructure = '';
-    onGetPost(post => {
-      post.forEach(doc => {
-        const posts = doc.data();
-        // console.log(posts)
-
-        postStructure += `
-        <div class='post'>
-        <hr/>
+    if (divFeed != '') {
+      onGetPost((post) => {
+        postContainer.innerHTML ='';
+        let postStructure = '';
+        post.forEach(doc => {
+          const posts = doc.data();
+          // console.log(posts)
+          
+          postStructure += `
+          <div class='post'>
           <p>username: <b>${posts.userName}</b></p>
           <h3>${posts.title}</h3>
           <p>${posts.description}</p>
-          <button class='btn-delete' data-id='${doc.id}'>Delete</button>
-          <button class='btn-edit' data-id='${doc.id}'>Edit</button>          
-        </div>
-        `
-      }); 
+          <div>
+          <button class='btn-delete btn' data-id='${doc.id}'>Delete</button>
+          <button class='btn-edit btn' data-id='${doc.id}'>Edit</button>          
+          </div>
+          </div>
+          `
+        }); 
+        
+        postContainer.innerHTML = postStructure;
+        // console.log(postContainer.innerHTML = postStructure);
+        
+        
+        
+        const btnDelete = postContainer.querySelectorAll('.btn-delete');
+        console.log(btnDelete);
+        btnDelete.forEach(btn => {
+          btn.addEventListener('click',  (e) => {
+            
+            //console.log(btn)
+            id = e.target.dataset.id;
+            //console.log(id);
+            deletePost(id)
+           
+              })
+            })
+            
+        const postBtn = divFeed.querySelector('#post-btn');
 
-      postContainer.innerHTML = postStructure;
-      // console.log(postContainer.innerHTML = postStructure);
-    
+        const btnEdit = divFeed.querySelectorAll('.btn-edit');
+       btnEdit.forEach( btn => {
+        btn.addEventListener('click',async (e) => {
+          const doc = await getPost(e.target.dataset.id)
+          console.log(doc)
+          const task = doc.data();
+          console.log(task)
+       postForm.querySelector('#title').value = task.title;
+       postForm.querySelector('#description').value = task.description;
+          
+          editStatus = true; 
+          id = e.target.dataset.id;
+          postBtn.innerText = 'Actualizar'
+
+          
+          });
+        
+        })
+          
+      });
+    }  
+        postForm.addEventListener('submit', (e) => {
+          e.preventDefault();
+          const title = divFeed.querySelector('#title').value;
+          const description = divFeed.querySelector('#description').value;
+         if(!editStatus){
+
+           createPost(title, description );
+         } else {
+           editPost(
+             id,{
+               title,
+
+               description,
+             }
+           )
+           editStatus =false;
+         }
+          
+          postForm.reset();
+        });
 
 
-      const btnDelete = postContainer.querySelectorAll('.btn-delete');
-      console.log(btnDelete);
-      btnDelete.forEach(btn => {
-        btn.addEventListener('click',  (e) => {
-
-          //console.log(btn)
-        id = e.target.dataset.id;
-          //console.log(id);
-        deletePost(id)
-      // postContainer.innerHTML =''
-      //   onGetPost(post => {
-      //     post.forEach(doc => {
-      //       const posts = doc.data();
-      //       // console.log(posts)
-    
-      //       postStructure += `
-      //       <div class='post'>
-      //       <hr/>
-      //         <p>username: <b>${posts.userName}</b></p>
-      //         <h3>${posts.title}</h3>
-      //         <p>${posts.description}</p>
-      //         <button class='btn-delete' data-id='${doc.id}'>Delete</button>
-      //         <button class='btn-edit' data-id='${doc.id}'>Edit</button>          
-      //       </div>
-      //       `
-      //     }); 
-    
-      //     postContainer.innerHTML = postStructure;
-
-      //   //console.log(deletePost(id), id);
-        // })
-      })
-    })
-    });
-
-    
-  };
 
 
 
   return divFeed;
 };
-
